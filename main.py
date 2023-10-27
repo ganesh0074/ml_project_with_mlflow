0dc3a06e7750110e8a38e37fc6c58dd2b2d8f2ec
Ganesh G <ganesh.gulhane007@gmail.com>
2023-10-27 15:37:14 +0530

    data_validation complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlproject import logger
 from mlproject.pipeline.stage_01_data_ingestion import dataingestiontrainingPipeline
+from mlproject.pipeline.stage_02_data_validation import dataValidationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -8,6 +9,19 @@ try:
     data_ingestion = dataingestiontrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>> stage  {STAGE_NAME} completed <<<<<<<") 
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+
+    logger.info(f">>>> stage {STAGE_NAME} started   <<<<")  
+    data_validation = dataValidationPipeline()
+    data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<") 
 except Exception as e:
     logger.exception(e)
     raise e 
---
+++ src/mlproject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,29 @@
+from mlproject.components.data_validation import DataValiadtion
+from mlproject.config.configuration import ConfigurationManager
+from mlproject import logger
+
+
+STAGE_NAME = "Data Validation Stage"
+
+class dataValidationPipeline:
+
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config= ConfigurationManager()
+        data_validation_config=config.get_data_validation_config()
+        data_validation= DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__=="__main__":
+    try:
+
+        logger.info(f">>>> stage {STAGE_NAME} started   <<<<")  
+        obj = dataValidationPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<") 
+    except Exception as e:
+        logger.exception(e)
+        raise e 
